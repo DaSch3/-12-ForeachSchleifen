b05e8b188a00c8575acc08534ec40989a6863de3
David Schmitz <d.schmitz@dasch3.de>
2021-09-04 19:30:00 +0200

    Projektdateien hinzufügen.

---
+++ ForeachSchleifen/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForeachSchleifen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] zahlenListe = new int[5];
+
+            zahlenListe[0] = 10;
+            zahlenListe[1] = 20;
+            zahlenListe[2] = 30;
+            zahlenListe[3] = 40;
+            zahlenListe[4] = 50;
+
+            foreach(int zahl in zahlenListe)
+            {
+                Console.WriteLine(zahl);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
